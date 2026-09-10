1440ce863932ee1cc7b4564009545497a11cf0ae
이승준 <seungjun@LeeSeungJunui-MacBookPro.local>
2026-09-10 15:41:03 +0900

    설치했었지만 안쓰이고 있던 Redux라이브러리 제거

--- src/main.tsx
+++ src/main.tsx
@@ -4,16 +4,11 @@ import './index.css';
 import App from './App';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 const queryClient = new QueryClient();
-import { store } from './store';
-import { Provider } from 'react-redux';
 
 createRoot(document.getElementById('root')!).render(
   <BrowserRouter>
     <QueryClientProvider client={queryClient}>
-      <Provider store={store}>
-        <App />
-      </Provider>
+      <App />
     </QueryClientProvider>
   </BrowserRouter>
 );
-
